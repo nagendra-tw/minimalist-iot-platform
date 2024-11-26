592824eadd3413206754f2252ead783c0368ff2f
Nagendra Babu <nagendrababu.marasu@thoughtworks.com>
2024-11-26 12:16:47 +0530

    Add. Project Setup

---
+++ src/main/java/com/learnings/iot_platform/IotPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.learnings.iot_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IotPlatformApplication.class, args);
+	}
+
+}

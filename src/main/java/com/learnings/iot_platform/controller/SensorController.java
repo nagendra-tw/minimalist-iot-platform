03604230b1fede5ee90aee86752224ac56d18e3d
Nagendra Babu <nagendrababu.marasu@thoughtworks.com>
2024-11-27 12:29:28 +0530

    Add. get all sensors in web layer

--- src/main/java/com/learnings/iot_platform/controller/SensorController.java
+++ src/main/java/com/learnings/iot_platform/controller/SensorController.java
@@ -7,16 +7,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/sensors")
 public class SensorController {
     @Autowired
     private SensorService sensorService;
+
+    @GetMapping
+    public ResponseEntity<List<SensorResponseDto>> getAllSensors() {
+        return new ResponseEntity<>(sensorService.getAllSensors(), HttpStatus.OK);
+    }
 
     @PostMapping
     public ResponseEntity<SensorResponseDto> createSensor(@RequestBody SensorRequestDto sensorRequestDto) {
--- src/test/java/com/learnings/iot_platform/controller/SensorControllerTest.java
+++ src/test/java/com/learnings/iot_platform/controller/SensorControllerTest.java
@@ -5,12 +5,15 @@ import com.learnings.iot_platform.dto.SensorRequestDto;
 import com.learnings.iot_platform.dto.SensorResponseDto;
 import com.learnings.iot_platform.service.SensorService;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -50,6 +53,27 @@ public class SensorControllerTest {
                 .andExpect(jsonPath("$.temperature").value(temperature))
                 .andExpect(jsonPath("$.latitude").value(latitude))
                 .andExpect(jsonPath("$.longitude").value(longitude))
+        ;
+    }
+
+    @Test
+    void shouldReturnAllSensors() throws Exception {
+        List<SensorResponseDto> sensorResponseDtoList = new ArrayList<>();
+        sensorResponseDtoList.add(new SensorResponseDto("1", "sensor1", 50, 17, 18));
+        sensorResponseDtoList.add(new SensorResponseDto("2", "sensor2", 50, 17, 18));
+        sensorResponseDtoList.add(new SensorResponseDto("3", "sensor3", 50, 17, 18));
+
+        when(sensorService.getAllSensors()).thenReturn(sensorResponseDtoList);
+
+        mockMvc.perform(get("/sensors"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(3))
+                .andExpect(jsonPath("$[0].id").value("1"))
+                .andExpect(jsonPath("$[0].name").value("sensor1"))
+                .andExpect(jsonPath("$[1].id").value("2"))
+                .andExpect(jsonPath("$[1].name").value("sensor2"))
+                .andExpect(jsonPath("$[2].id").value("3"))
+                .andExpect(jsonPath("$[2].name").value("sensor3"))
         ;
     }
 
